53847283e014108568a46f8150ba7a3db4feec41
Leonardo Hirooka <leonardohirooka@hotmail.com>
2023-03-29 01:32:20 -0300

    Javascript OO 3

--- javascript/OO/luta/js/classes.js
+++ javascript/OO/luta/js/classes.js
@@ -24,7 +24,7 @@ class Knight extends Character {
         super(name);
         this.life = 100;
         this.attack = 10;
-        this.defense = 8;
+        this.defense = 10;
         this.maxLife = this.life;
     }
 }
@@ -61,23 +61,72 @@ class BigMonster extends Character {
 }
 
 class Stage {
-    constructor(fighter1, fighter2, fighter1El, fighter2El) {
+    constructor(fighter1, fighter2, fighter1El, fighter2El, logObj) {
         this.fighter1 = fighter1;
         this.fighter2 = fighter2;
         this.fighter1El = fighter1El;
         this.fighter2El = fighter2El;
+        this.log = logObj;
 
     }
 
     start() {
         this.update();
+        this.fighter1El.querySelector('.attackButton').addEventListener('click', () => this.doAttack(this.fighter1, this.fighter2));
+        this.fighter2El.querySelector('.attackButton').addEventListener('click', () => this.doAttack(this.fighter2, this.fighter1));
     }
 
     update() {
         // Fighter 1
-        this.fighter1El.querySelector('.name').innerHTML = this.fighter1.name;
+        this.fighter1El.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life.toFixed(2)}HP`;
+        let f1pct = (this.fighter1.life / this.fighter1.maxLife) * 100;
+        this.fighter1El.querySelector('.bar').style.width = `${f1pct}%`;
+
         // Fighter 2
-        this.fighter2El.querySelector('.name').innerHTML = this.fighter2.name;
+        this.fighter2El.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life.toFixed(2)}HP`;
+        let f2pct = (this.fighter2.life / this.fighter2.maxLife) * 100;
+        this.fighter2El.querySelector('.bar').style.width = `${f2pct}%`;
     }
 
+    doAttack(attacking, attacked) {
+        if (attacking.life <= 0 || attacked.life <= 0) {
+            this.log.addMessage('Chutando cadaver'); return;
+        }
+        let attackFactor = (Math.random() * 2).toFixed(2);
+        let defenseFactor = (Math.random() * 2).toFixed(2);
+
+        let actualAttack = attacking.attack * attackFactor;
+        let actualDefense = attacked.defense * defenseFactor;
+
+        if (actualAttack > actualDefense) {
+            attacked.life = attacked.life - actualAttack;
+            this.log.addMessage(`${attacking.name} causou ${actualAttack} de dano em ${attacked.name}`)
+
+        } else { this.log.addMessage(`${attacked.name} Defendeu`) }
+
+
+
+        this.update();
+    }
+
+}
+
+class Log {
+    list = [];
+
+    constructor(listEl) {
+        this.listEl = listEl;
+    }
+
+    addMessage(msg) {
+        this.list.push(msg);
+        this.render();
+    }
+
+    render() {
+        this.listEl.innerHTML = ' ';
+        for (let i in this.list) {
+            this.listEl.innerHTML += `<li> ${this.list[i]} </li>`
+        }
+    }
 }
--- javascript/OO/luta/js/script.js
+++ javascript/OO/luta/js/script.js
@@ -1,14 +1,17 @@
+let log = new Log(document.querySelector('.log'));
+
 let char = new Knight('Cavaleiro Solaire');
 console.log(char);
 
-let monster = new Monster();
+let monster = new BigMonster();
 console.log(monster);
 
 const stage = new Stage(
     char,
     monster,
     document.querySelector('#char'),
-    document.querySelector('#monster')
+    document.querySelector('#monster'),
+    log,
 );
 
 stage.start();
